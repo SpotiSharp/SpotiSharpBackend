9d367c9e89a38577194d112049855726fc90abc1
Teddy <teddy@teddy.email>
2023-01-08 16:05:55 +0100

    add SongData Model

---
+++ SongData.cs
@@ -0,0 +1,9 @@
+﻿using SpotifyAPI.Web;
+
+namespace SpotiSharpBackend;
+
+public class SongData
+{
+    public FullTrack? FullTrack { get; set; } = new FullTrack();
+    public TrackAudioFeatures? AudioFeatures { get; set; } = new TrackAudioFeatures();
+}
